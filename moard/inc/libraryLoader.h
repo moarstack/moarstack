3d190dfd7ae62db8bccb91a17ea833e0df166c1a
svalov <test>
2016-07-02 16:22:19 +0200

    Added MoarLibrary structure Added function prototype

---
+++ moard/inc/libraryLoader.h
@@ -0,0 +1,25 @@
+//
+// Created by svalov on 7/2/16.
+//
+
+#ifndef MOARSTACK_LIBRARYLOADER_H
+#define MOARSTACK_LIBRARYLOADER_H
+
+#include <moarLibInterface.h>
+#include <moarLayerEntryPoint.h>
+
+#define LIBRARY_LOAD_OK 0
+#define LIBRARY_LOAD_FAILED 1
+#define LIBRARY_LOAD_NONMOAR 2
+#define LIBRARY_LOAD_NONLAYER 3
+
+typedef struct{
+    char* Filename;
+    MoarLibInfo_T Info;
+    moarLibraryInfo_F LibraryInfoFunction;
+    moarLayerEntryPoint_F LayerEntryPointFunction;
+} MoarLibrary_T;
+
+int loadLibrary(char* name, MoarLibrary_T* library);
+
+#endif //MOARSTACK_LIBRARYLOADER_H
---
+++ moard/src/libraryLoader.c
@@ -0,0 +1,8 @@
+//
+// Created by svalov on 7/2/16.
+//
+#include <libraryLoader.h>
+
+int loadLibrary(char* name, MoarLibrary_T* library){
+    return LIBRARY_LOAD_FAILED;
+}

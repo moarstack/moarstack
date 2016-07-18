d31f79e574b4a20c0d38aec260ef761b150be7fd
svalov <test>
2016-07-18 18:18:46 +0200

    Add some comments to channel/src/moarChannel.c

--- layers/channel/src/moarChannel.c
+++ layers/channel/src/moarChannel.c
@@ -10,5 +10,70 @@
 #include "moarChannelPrivate.h"
 
 void * MOAR_LAYER_ENTRY_POINT(void* arg){
+    // listen for interface connection
+    // in poll
+        // if new interface connected
+            // add to pool list
+            // if supported beacon payload
+                // update beacon data
+                // update beacon data in all interfaces
+
+        // if command from interface
+
+            // connect
+                //a dd interface record to list
+
+            // disconnect
+                // remove interface record from list
+
+            // interface/message state
+                // change state of interface
+                // send message state command to routing
+                // if have messages in queue
+                    // get message
+                    // try to send
+
+            // received
+                // if contains neighbors info metadata | hello message
+                    // update neighbor information
+                // if contains payload
+                    // send command to routing
+                // else
+                    // drop message
+
+            // new neighbor
+                // if contains beacon payload
+                    //if found neighbor
+                        //update neighbor
+                    //else
+                        // add new neighbor
+                        // send to routing new neighbor command
+                // else
+                    // add to queue hello message
+
+            // lost neighbor
+                // if found neighbor
+                    // remove interface info
+                    // if no interfaces
+                        // remove neighbor
+                        // send to routing lost neighbor command
+
+            // update neighbor
+                // ?????
+                // PROFIT
+
+        // if command from routing
+
+            // send
+                // if found neighbor
+                    // if found free interface
+                        // push data to interface
+                    // else
+                        // if queue is not full
+                            // add to queue
+                        // else
+                            // send error to routing
+                // else
+                    // send error to routing
 
 }

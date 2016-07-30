1d914f2b492f09fac26751903556c9c1bdca4aef
Kryvashek <kryvashek@gmail.com>
2016-07-30 12:40:18 +0200

    Added MessageId_T structure and some related defines
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- layers/shared/inc/moarMessageId.h
+++ layers/shared/inc/moarMessageId.h
@@ -5,4 +5,18 @@
 #ifndef MOARSTACK_MOARMESSAGEID_H
 #define MOARSTACK_MOARMESSAGEID_H
 
+#include <moarLibInterface.h> // MoarLayerType_T
+
+#define MESSAGE_ID_FREE_SIZE	7
+#define MESSAGE_ID_FULL_SIZE	sizeof( MessageId_T )
+
+#pragma pack(push, 1)
+
+typedef struct {
+	MoarLayerType_T	SourceLayer;
+	uint8_t			FreePart[ MESSAGE_ID_FREE_SIZE ];
+} MessageId_T ;
+
+#pragma pack(pop)
+
 #endif //MOARSTACK_MOARMESSAGEID_H

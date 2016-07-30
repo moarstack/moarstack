b7f20fcea87efa27f30c8a4cc8711655664ff4a2
Kryvashek <kryvashek@gmail.com>
2016-07-30 13:04:20 +0200

    Implemented function for generating message identifier midGenerate().
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- layers/shared/src/moarMessageId.c
+++ layers/shared/src/moarMessageId.c
@@ -6,6 +6,8 @@
 
 #include <moarMessageId.h>
 
+static uint8_t	value[ MESSAGE_ID_FULL_SIZE ] = { 0 };
+
 // checks whether given message ids are equal; returns true, if are, false otherwise
 bool midAreEqual( MessageId_T * one, MessageId_T * two ) {
 	if( ( NULL != one && NULL == two ) || ( NULL == one && NULL != two ) )
@@ -16,7 +18,31 @@ bool midAreEqual( MessageId_T * one, MessageId_T * two ) {
 		return ( 0 == memcmp( one, two, MESSAGE_ID_FULL_SIZE ) );
 }
 
+int increment( uint8_t * valueArray ) {
+	if( NULL == valueArray )
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	valueArray[ MESSAGE_ID_FULL_SIZE - 1 ]++;
+
+	return FUNC_RESULT_SUCCESS;
+}
+
 // generates new identifier for some packet
 int midGenerate( MessageId_T * identifier, MoarLayerType_T layer ) {
+	int result;
+
+	if( NULL == identifier )
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	result = increment( value );
+
+	if( FUNC_RESULT_SUCCESS != result )
+		return FUNC_RESULT_FAILED;
+
+	memcmp( identifier, value, MESSAGE_ID_FULL_SIZE );
+
+	if( MoarLayer_LayersCount > layer )
+		identifier->SourceLayer = layer;
+
 	return FUNC_RESULT_SUCCESS;
 }

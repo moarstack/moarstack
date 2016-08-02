8ce348457a13d7e3354c3ea9a03da5c18eec08c6
svalov <test>
2016-08-02 13:05:17 +0200

    Added internal channel structs Added linked list

--- layers/channel/inc/moarChannelPrivate.h
+++ layers/channel/inc/moarChannelPrivate.h
@@ -5,13 +5,73 @@
 #ifndef MOARSTACK_MOARCHANNELPRIVATE_H
 #define MOARSTACK_MOARCHANNELPRIVATE_H
 
+#include <time.h>
+#include <stdint.h>
+#include <moarChannel.h>
 #include <moarUnIfaceAddr.h>
+#include <sys/epoll.h>
+#include <linkedList.h>
+#include <moarChannelRouting.h>
 
-// metadata of packet moving from channel to interface
+#define LISTEN_COUNT						10
+#define EPOLL_INTERFACE_SOCKET_EVENTS 		EPOLLIN
+#define EPOLL_INTERFACE_EVENTS 				EPOLLIN
+#define EPOLL_INTERFACE_DISCONNECT_EVENTS 	(EPOLLERR | EPOLLHUP)
+#define EPOLL_ROUNTING_EVENTS 				EPOLLIN
+#define EPOLL_TIMEOUT						1000
+#define MAX_INTERFACE_COUNT					255
+#define EPOLL_EVENTS_COUNT					(MAX_INTERFACE_COUNT+2)
+#define SEND_TRYS							5
+
 typedef struct {
-	UnIfaceAddr_T	Bridge;
-} ChannelMsgDown_T;
+	UnIfaceAddr_T 	Address;
+	time_t 			LastSeen;
+} InterfaceNeighbor_T;
 
-#define	CHANNEL_MSG_DOWN_SIZE  sizeof( ChannelMsgDown_T )
+typedef struct {
+	UnIfaceAddr_T Address;
+	bool Ready;
+	//PackStateIface_T CurrentState;
+	int Socket;
+	InterfaceNeighbor_T* Neighbors;
+	// pointer to channel neighbor
+} InterfaceDescriptor_T;
+
+typedef struct{
+	UnIfaceAddr_T Address;
+	InterfaceDescriptor_T* Interface;
+} RemoteInterface_T;
+
+typedef struct{
+	ChannelAddr_T 	RemoteAddress;
+	time_t 			LastSeen;
+	LinkedListItem_T Interfaces;
+} ChannelNeighbor_T;
+
+typedef struct{
+	//processing time
+	int8_t TrysLeft;
+	RouteSendMetadata_T Metadata;
+	PayloadSize_T  DataSize;
+	void* Data;
+} ChannelMessageEntry_T;
+
+typedef struct {
+	ChannelAddr_T 			LocalAddress;
+	int 					UpSocket;
+	int 					DownSocket;
+	uint8_t 				InterfacesCount;
+	LinkedListItem_T 		Interfaces;
+	LinkedListItem_T 		Neighbors;
+	int 					EpollHandler;
+	struct epoll_event 		EpollEvent[EPOLL_EVENTS_COUNT];
+	int 					EpollCount;
+	bool 					Running;
+	int 					EpollTimeout;
+	//mesage state table
+	//some message queue
+} ChannelLayer_T;
+
+int processCloseConnection(ChannelLayer_T* layer, int fd);
 
 #endif //MOARSTACK_MOARCHANNELPRIVATE_H

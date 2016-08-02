953747682e6ce23cb51b48bda4f4d02fc147e5b3
Kryvashek <kryvashek@gmail.com>
2016-08-02 14:13:41 +0200

    Crosslayer socket routine corrected. Common SocketOpenFile() used.
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- moard/inc/layerSockets.h
+++ moard/inc/layerSockets.h
@@ -5,12 +5,11 @@
 #ifndef MOARSTACK_LAYERSOCKETS_H
 #define MOARSTACK_LAYERSOCKETS_H
 
-#include "moarLibInterface.h"	// MoarLayerType_T
-#include "moarCommons.h"		// SocketFilepath_T
+#include <moarLibInterface.h>	// MoarLayerType_T
+#include <moarCommons.h>		// SocketFilepath_T
 
-int socketDown( MoarLayerType_T layerType );
-int socketUp( MoarLayerType_T layerType );
-int socketOpenFile( const SocketFilepath_T socketFilePath );
-int socketsPrepare( const SocketFilepath_T ifaceSocketFilePath, const SocketFilepath_T serviceSocketFilePath  );
+int SocketDown( MoarLayerType_T layerType );
+int SocketUp( MoarLayerType_T layerType );
+int SocketsPrepare( const SocketFilepath_T ifaceSocketFilePath, const SocketFilepath_T serviceSocketFilePath  );
 
 #endif //MOARSTACK_LAYERSOCKETS_H

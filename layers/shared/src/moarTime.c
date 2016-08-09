b753d3db7b1d3d1fc2e95fe8a89148d322d54052
svalov <test>
2016-08-09 15:32:29 +0200

    Added header for time Added linking with rt

---
+++ layers/shared/src/moarTime.c
@@ -0,0 +1,18 @@
+//
+// Created by svalov on 8/9/16.
+//
+
+#include <moarTime.h>
+
+moarTime_T timeGetCurrent(){
+	return 0;
+}
+moarTimeInterval_T timeGetDifference(moarTime_T first, moarTime_T second){
+	return 0;
+}
+int timeCompare(moarTime_T first, moarTime_T second){
+	return 0;
+}
+moarTime_T timeAddInterval(moarTime_T time, moarTimeInterval_T interval){
+	return 0;
+}

27f157d288b65d4dd2427d20e803b087c0a560c1
svalov <test>
2016-08-13 15:18:53 +0200

    Added command reading from socket

--- layers/routing/src/moarRouitngCommandProcessing.c
+++ layers/routing/src/moarRouitngCommandProcessing.c
@@ -7,8 +7,56 @@
 #include "moarRouitngCommandProcessing.h"
 
 int processChannelEvent(RoutingLayer_T* layer, int fd, uint32_t event){
-	return FUNC_RESULT_FAILED;
+	if(NULL == layer)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(fd <= 0)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	if((event & EPOLL_CHANNEL_EVENTS) !=0) {
+		// send
+		LayerCommandStruct_T command = {0};
+		int commandRes = ReadCommand(fd, &command);
+		if (FUNC_RESULT_SUCCESS != commandRes) {
+			return commandRes;
+		}
+		int res = FUNC_RESULT_FAILED;
+		switch (command.Command) {
+			case LayerCommandType_Receive:
+				// res = processSendMessage(layer, fd, &command);
+				break;
+			default:
+				res = FUNC_RESULT_FAILED_ARGUMENT;
+				break;
+		}
+		FreeCommand(&command);
+		return res;
+	}
+	return FUNC_RESULT_SUCCESS;
 }
 int processPresentationEvent(RoutingLayer_T* layer, int fd, uint32_t event){
-	return FUNC_RESULT_FAILED;
+	if(NULL == layer)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(fd <= 0)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	if((event & EPOLL_PRESENTATION_EVENTS) !=0) {
+		// send
+		LayerCommandStruct_T command = {0};
+		int commandRes = ReadCommand(fd, &command);
+		if (FUNC_RESULT_SUCCESS != commandRes) {
+			return commandRes;
+		}
+		int res = FUNC_RESULT_FAILED;
+		switch (command.Command) {
+				case LayerCommandType_Send:
+				//res = processSendMessage(layer, fd, &command);
+				break;
+			default:
+				res = FUNC_RESULT_FAILED_ARGUMENT;
+				break;
+		}
+		FreeCommand(&command);
+		return res;
+	}
+	return FUNC_RESULT_SUCCESS;
 }

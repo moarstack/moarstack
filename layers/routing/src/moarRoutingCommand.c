e41f02b677381fc212f3524c1915553a8c9924c3
Kryvashek <kryvashek@gmail.com>
2016-08-18 21:25:07 +0200

    Started implementation of processSendCommand().
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- layers/routing/src/moarRoutingCommand.c
+++ layers/routing/src/moarRoutingCommand.c
@@ -4,7 +4,7 @@
 
 #include <moarRoutingPrivate.h>
 #include <funcResults.h>
-#include "moarRoutingCommand.h"
+#include <moarRoutingCommand.h>
 
 int processReceiveCommand(void* layerRef, int fd, LayerCommandStruct_T* command){
 	if(NULL == layerRef)
@@ -65,14 +65,27 @@ int processUpdateNeighborCommand(void* layerRef, int fd, LayerCommandStruct_T* c
 	//logic here
 	return FUNC_RESULT_SUCCESS;
 }
-int processSendCommand(void* layerRef, int fd, LayerCommandStruct_T* command){
-	if(NULL == layerRef)
+
+int processSendCommand( void * layerRef, int fd, LayerCommandStruct_T * command ) {
+	int						result;
+	RoutingLayer_T			* layer;
+	PresentSendMetadata_T	* metadata;
+	RouteStoredPacket_T		storedPacket;
+
+	if( NULL == layerRef || fd <= 0 || NULL == command || 0 == command->MetaSize || NULL == command->MetaData )
 		return FUNC_RESULT_FAILED_ARGUMENT;
-	if(fd <= 0)
-		return FUNC_RESULT_FAILED_ARGUMENT;
-	if(NULL == command)
-		return FUNC_RESULT_FAILED_ARGUMENT;
-	RoutingLayer_T* layer = (RoutingLayer_T*)layerRef;
-	//logic here
+
+	layer = ( RoutingLayer_T * )layerRef;
+	metadata = ( PresentSendMetadata_T * )command->MetaData;
+
+	if( 0 == command->DataSize || NULL == command->Data ) { // if no meaningfull data
+		// inform presentation about not sent message
+	} else { //
+		storedPacket.Destination = metadata->Destination;
+		storedPacket.InternalId = metadata->Id;
+		result = rmidGenerate( &( storedPacket.MessageId ) );
+	}
+
+
 	return FUNC_RESULT_SUCCESS;
 }

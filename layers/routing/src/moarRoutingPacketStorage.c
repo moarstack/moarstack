6a060239f84c9520591106e971854c5203d5a9ab
svalov <test>
2016-09-14 19:13:27 +0200

    Added Add function

--- layers/routing/src/moarRoutingPacketStorage.c
+++ layers/routing/src/moarRoutingPacketStorage.c
@@ -8,6 +8,8 @@
 #include <moarMessageId.h>
 #include <moarRoutingMessageId.h>
 #include <moarRoutingPacketStorage.h>
+#include <memory.h>
+#include <moarRoutingStoredPacket.h>
 
 int timeCompareFunction(void* k1, void* k2, size_t size){
 	if(k1 == NULL)
@@ -25,22 +27,22 @@ int psInit(PacketStorage_T* storage){
 
 	//init queue
 	int queueRes = pqInit(&(storage->NextProcessingTime), QUEUE_SIZE, timeCompareFunction,
-						  sizeof(moarTime_T), sizeof(RouteStoredPacket_T));
+						  sizeof(moarTime_T), sizeof(RouteStoredPacket_T*));
 	if(FUNC_RESULT_SUCCESS != queueRes)
 		return queueRes;
 	//init dest table
 	int destRes = hashInit(&(storage->Destinations), hashRoutingAddress, DEST_HASH_SIZE,
-						   sizeof(RouteAddr_T), sizeof(RouteStoredPacket_T));
+						   sizeof(RouteAddr_T), sizeof(RouteStoredPacket_T*));
 	if(FUNC_RESULT_SUCCESS != destRes)
 		return destRes;
 	//init mid table
 	int midRes = hashInit(&(storage->MessageIds), midHash, MID_HASH_SIZE,
-						   sizeof(MessageId_T), sizeof(RouteStoredPacket_T));
+						   sizeof(MessageId_T), sizeof(RouteStoredPacket_T*));
 	if(FUNC_RESULT_SUCCESS != midRes)
 		return midRes;
 	//init rmid table
 	int rmidRes = hashInit(&(storage->RoutingMessageIds), rmidHash, RMID_HASH_SIZE,
-						  sizeof(RoutingMessageId_T), sizeof(RouteStoredPacket_T));
+						  sizeof(RoutingMessageId_T), sizeof(RouteStoredPacket_T*));
 	if(FUNC_RESULT_SUCCESS != rmidRes)
 		return rmidRes;
 
@@ -73,4 +75,60 @@ int psDeinit(PacketStorage_T* storage){
 	storage->Count = 0;
 
 	return FUNC_RESULT_SUCCESS;
+}
+
+int psAdd(PacketStorage_T* storage, RouteStoredPacket_T* packet){
+	if(NULL == storage)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(NULL == packet)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	// allocate memory and copy
+	RouteStoredPacket_T* allocatedPack = malloc(sizeof(RouteStoredPacket_T));
+	if(NULL == allocatedPack)
+		return FUNC_RESULT_FAILED_MEM_ALLOCATION;
+	memcpy(allocatedPack, packet, sizeof(RouteStoredPacket_T));
+
+	// add to queue
+	int queueRes = pqEnqueue(&(storage->NextProcessingTime), &(allocatedPack->NextProcessing), &allocatedPack);
+	// add to dest
+	int destRes = hashAdd(&(storage->Destinations), &(allocatedPack->Destination), &allocatedPack);
+	// add to mid
+	int midRes = hashAdd(&(storage->MessageIds), &(allocatedPack->InternalId), &allocatedPack);
+	// add to rmid
+	int rmidRes = hashAdd(&(storage->RoutingMessageIds), &(allocatedPack->MessageId), &allocatedPack);
+
+	//todo think about errors while adding
+
+	if(FUNC_RESULT_SUCCESS != queueRes ||
+	   		FUNC_RESULT_SUCCESS != destRes ||
+			FUNC_RESULT_SUCCESS != midRes ||
+			FUNC_RESULT_SUCCESS != rmidRes){
+		//remove
+		pqRemove(&(storage->NextProcessingTime), &allocatedPack);
+		if(FUNC_RESULT_SUCCESS == destRes)
+			hashRemove(&(storage->Destinations), &(allocatedPack->Destination));
+		// TODO need remove exact function
+		hashRemove(&(storage->MessageIds), &(allocatedPack->InternalId));
+		hashRemove(&(storage->RoutingMessageIds), &(allocatedPack->MessageId));
+		// free memory
+		free(allocatedPack);
+		return FUNC_RESULT_FAILED;
+	}
+
+	return FUNC_RESULT_SUCCESS;
+}
+
+int psRemove(PacketStorage_T* storage, RouteStoredPacket_T* packet){
+	if(NULL == storage)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(NULL == packet)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	RouteStoredPacket_T* allocatedPack;
+	// find pack by mid
+	// if can not find - search by rmid
+
+	//
+
 }

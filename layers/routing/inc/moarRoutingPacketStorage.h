291d00f47d57ff074c828113410c9b5ef2c97d7c
svalov <test>
2016-09-14 17:41:23 +0200

    Added functions signatures Added init func body

--- layers/routing/inc/moarRoutingPacketStorage.h
+++ layers/routing/inc/moarRoutingPacketStorage.h
@@ -6,5 +6,42 @@
 #define MOARSTACK_MOARROUTINGPACKETSTORAGE_H
 
 #include <moarRoutingStoredPacket.h>
+#include <hashTable.h>
+#include <priorityQueue.h>
+
+#define RMID_HASH_SIZE 	37
+#define MID_HASH_SIZE 	37
+#define DEST_HASH_SIZE 	37
+#define QUEUE_SIZE 		32
+
+// structure
+typedef struct{
+	int 			Count;
+	PriorityQueue_T NextProcessingTime;
+	hashTable_T		Destinations;
+	hashTable_T		MessageIds;
+	hashTable_T		RoutingMessageIds;
+}PacketStorage_T;
+
+// init
+int psInit(PacketStorage_T* storage);
+// deinit
+int psDeinit(PacketStorage_T* storage);
+// add
+int psAdd(PacketStorage_T* storage, RouteStoredPacket_T* packet);
+// remove
+int psRemove(PacketStorage_T* storage, RouteStoredPacket_T* packet);
+// search
+int psGetDest(PacketStorage_T* storage, RouteAddr_T* dest, RouteStoredPacket_T* packet);
+int psGetMid(PacketStorage_T* storage, MessageId_T* mid, RouteStoredPacket_T* packet);
+int psGetRmid(PacketStorage_T* storage, RoutingMessageId_T* rmid, RouteStoredPacket_T* packet);
+
+RouteStoredPacket_T* psGetDestPtr(PacketStorage_T* storage, RouteAddr_T* dest);
+RouteStoredPacket_T* psGetMidPtr(PacketStorage_T* storage, MessageId_T* mid);
+RouteStoredPacket_T* psGetRmidPtr(PacketStorage_T* storage, RoutingMessageId_T* rmid);
+
+// get top
+RouteStoredPacket_T* psGetTop(PacketStorage_T* storage);
+
 
 #endif //MOARSTACK_MOARROUTINGPACKETSTORAGE_H
--- layers/routing/src/moarRoutingPacketStorage.c
+++ layers/routing/src/moarRoutingPacketStorage.c
@@ -3,5 +3,16 @@
 //
 
 #include "moarRoutingPacketStorage.h"
+#include <funcResults.h>
+#include <hashFunc.h>
+#include <moarMessageId.h>
+#include <moarRoutingMessageId.h>
+
+int psInit(PacketStorage_T* storage){
+	if(NULL == storage)
+		return FUNC_RESULT_FAILED_ARGUMENT;
 
 
+
+	return FUNC_RESULT_SUCCESS;
+}

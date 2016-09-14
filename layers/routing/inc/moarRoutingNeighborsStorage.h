e02346d9f6aa5a6a7663df9116ce340122708a39
svalov <test>
2016-09-14 14:23:34 +0200

    Added base api functions

--- layers/routing/inc/moarRoutingNeighborsStorage.h
+++ layers/routing/inc/moarRoutingNeighborsStorage.h
@@ -6,13 +6,24 @@
 #define MOARSTACK_MOARROUTINGNEIGHBORSSTORAGE_H
 
 #include <moarRoutingPrivate.h>
+#include <hashTable.h>
+
+#define STORAGE_HASH_SIZE 41
 
 typedef struct{
 	RouteAddr_T Address;
 }RoutingNeighborInfo_T;
 
 typedef struct{
-	
+
 }RoutingNeighborsStorage_T;
+
+int storageInit(RoutingNeighborsStorage_T* storage);
+int storageDeinit(RoutingNeighborsStorage_T* storage);
+int storageAdd(RoutingNeighborsStorage_T* storage, RoutingNeighborInfo_T* info);
+int storageGet(RoutingNeighborsStorage_T* storage, RouteAddr_T* address, RoutingNeighborInfo_T* info);
+RoutingNeighborInfo_T* storageGetPtr(RoutingNeighborsStorage_T* storage, RouteAddr_T* address);
+int storageRemove(RoutingNeighborsStorage_T* storage, RouteAddr_T* address);
+int storageIterator(RoutingNeighborsStorage_T* storage, hashIterator_T* iterator);
 
 #endif //MOARSTACK_MOARROUTINGNEIGHBORSSTORAGE_H

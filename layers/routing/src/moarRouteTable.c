8cc27a6fa02e692bf54d18cf997c57e50b2c46f2
hydrophis-dpiralis <green.snake.opex@gmail.com>
2016-09-24 17:37:54 +0300

    review comments fix

--- layers/routing/src/moarRouteTable.c
+++ layers/routing/src/moarRouteTable.c
@@ -115,7 +115,7 @@ bool RouteTableDelAll(RouteDataTable_T *table, RouteAddr_T relay, RouteAddr_T de
     return ( count > 0 );
 }
 
-inline static void RouteTableUpdate(RouteDataRecord_T *row) {
+void RouteTableUpdate(RouteDataRecord_T *row) {
     const RouteChance_T	half = ( ~( RouteChance_T )0 ) >> 1; // this way we find half of maximum value for P assuming it is not negative
     row->P = ( row->P >> 1 ) + half;
     //table->LastTimeUpdated = tick; //add this line! TODO
@@ -203,22 +203,22 @@ bool Bump(RouteDataTable_T *table, RouteAddr_T relay, RouteChance_T newP) {
     return ( count > 0 );
 }
 
-inline static RouteDataRecord_T *RouteTableRowFirst(RouteDataTable_T *table) {
+RouteDataRecord_T *RouteTableRowFirst(RouteDataTable_T *table) {
     return ( NULL == table || 0 == table->Count ) ? NULL : table->Table;
 }
 
-inline static RouteDataRecord_T *RouteTableRowNext(RouteDataTable_T *table, RouteDataRecord_T *prevRow) {
+RouteDataRecord_T *RouteTableRowNext(RouteDataTable_T *table, RouteDataRecord_T *prevRow) {
     if( NULL == table || 0 == table->Count || NULL == prevRow )
         return NULL;
     else
         return ( ++prevRow >= ( table->Table + table->Count ) ) ? NULL : prevRow;
 }
 
-inline static RouteDataRecord_T *RouteTableRowIndexed(RouteDataTable_T *table, RouteTableSize_T index) {
+RouteDataRecord_T *RouteTableRowIndexed(RouteDataTable_T *table, RouteTableSize_T index) {
     return ( NULL == table || index >= table->Count ) ? NULL : ( table->Table + index );
 }
 
-inline static RouteDataTable_T *RouteTableCreate(RouteTableSize_T tableSize) {
+RouteDataTable_T *RouteTableCreate(RouteTableSize_T tableSize) {
     RouteDataTable_T * table = ( RouteDataTable_T * )malloc( sizeof( RouteDataTable_T ) );
     RouteTableInit( table, tableSize );
     return table;

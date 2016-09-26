d31b37cd3bed49d521dcd5ab17f2d6fbd86a59ce
svalov <test>
2016-09-26 16:30:48 +0200

    Added init, deinit and clear logic

--- layers/shared/inc/queue.h
+++ layers/shared/inc/queue.h
@@ -24,7 +24,8 @@ typedef struct{
 
 int queueInit(Queue_T* queue, size_t dataSize);
 int queueDenit(Queue_T* queue);
-int Enqueue(Queue_T* queue, void* data);
-int Dequeue(Queue_T* queue, void* data);
-int Peek(Queue_T* queue, void* data);
-void* PeekPtr(Queue_T* queue);
+int queueClear(Queue_T* queue);
+int queueEnqueue(Queue_T* queue, void* data);
+int queueDequeue(Queue_T* queue, void* data);
+int queuePeek(Queue_T* queue, void* data);
+void* queuePeekPtr(Queue_T* queue);
--- layers/shared/src/queue.c
+++ layers/shared/src/queue.c
@@ -2,12 +2,37 @@
 // Created by svalov on 9/26/16.
 //
 
-#include "queue.h"
 #include <funcResults.h>
+#include <queue.h>
 
-int queueInit(Queue_T* queue, size_t dataSize){}
-int queueDenit(Queue_T* queue){}
-int Enqueue(Queue_T* queue, void* data){}
-int Dequeue(Queue_T* queue, void* data){}
-int Peek(Queue_T* queue, void* data){}
-void* PeekPtr(Queue_T* queue){}
+int queueInit(Queue_T* queue, size_t dataSize){
+	if(NULL == queue)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(0 == dataSize)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	queue->Count = 0;
+	queue->DataSize = dataSize;
+	queue->Head = NULL;
+	queue->Tail = NULL;
+	return FUNC_RESULT_SUCCESS;
+}
+int queueDenit(Queue_T* queue){
+	if(NULL == queue)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	int res = queueClear(queue);
+	return res;
+}
+int queueClear(Queue_T* queue){
+	if(NULL == queue)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	while(queue->Count != 0){
+		int res = queueDequeue(queue, NULL);
+		if(FUNC_RESULT_SUCCESS != res)
+			return res;
+	}
+	return FUNC_RESULT_SUCCESS;
+}
+int queueEnqueue(Queue_T* queue, void* data){}
+int queueDequeue(Queue_T* queue, void* data){}
+int queuePeek(Queue_T* queue, void* data){}
+void* queuePeekPtr(Queue_T* queue){}

78f767be2b115881d56e4a2985c15a5d3f69e91c
hydrophis-dpiralis <green.snake.opex@gmail.com>
2016-11-05 16:17:47 +0200

    Packet creation

--- layers/routing/inc/moarRouteFinder.h
+++ layers/routing/inc/moarRouteFinder.h
@@ -5,4 +5,21 @@
 #ifndef MOARSTACK_MOARROUTEFINDER_H
 #define MOARSTACK_MOARROUTEFINDER_H
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <moarCommons.h>
+#include <moarTime.h>
+#include <funcResults.h>
+#include <moarRouting.h>
+#include <moarRoutingStoredPacket.h>
+#include "moarRouteFinder.h"
+
 #endif //MOARSTACK_MOARROUTEFINDER_H
+
+typedef struct
+{
+    RouteAddr_T        NodeList[];
+    int                Size;
+} RoutePayloadFinder_T;
+
+int produceRouteFinder(RoutingLayer_T *layer);
--- layers/routing/src/moarRouteFinder.c
+++ layers/routing/src/moarRouteFinder.c
@@ -2,4 +2,32 @@
 // Created by spiralis on 05.11.16.
 //
 
-#include "moarRouteFinder.h"
+#include <moarRoutingPrivate.h>
+#include <moarRouteFinder.h>
+
+
+int produceRouteFinder(RoutingLayer_T *layer){
+    if (NULL == layer)
+        return FUNC_RESULT_FAILED_ARGUMENT;
+
+    RouteStoredPacket_T packet = {0};
+
+    midGenerate(&packet.InternalId, MoarLayer_Routing);
+    rmidGenerate(&packet.MessageId);
+
+    packet.Source = layer->LocalAddress;
+    packet.NextProcessing = 0;
+    packet.PackType = RoutePackType_Ack;
+    packet.State = StoredPackState_InProcessing;
+    packet.TrysLeft = DEFAULT_ROUTE_TRYS;
+    packet.PayloadSize = sizeof(RoutePayloadFinder_T);
+    packet.Payload = malloc(packet.PayloadSize);
+    if (NULL == packet.Payload) return FUNC_RESULT_FAILED_MEM_ALLOCATION;
+
+    // todo fill payload
+
+
+
+
+    return psAdd(&layer->PacketStorage, &packet);
+}

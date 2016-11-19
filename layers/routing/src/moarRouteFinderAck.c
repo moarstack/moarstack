02193f06378785cdcc96d487ba2f8622948c4f57
Kryvashek <kryvashek@gmail.com>
2016-11-19 16:24:57 +0200

    Improved routes updating in processPayloadFinderAck().
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- layers/routing/src/moarRouteFinderAck.c
+++ layers/routing/src/moarRouteFinderAck.c
@@ -5,36 +5,47 @@
 #include <moarRouting.h>
 #include <moarRoutingTablesHelper.h>
 #include <funcResults.h>
+#include <moarRoutingPrivate.h>
+#include <moarRoutingStoredPacket.h>
 #include "moarRouteFinderAck.h"
 
 int processPayloadFinderAck( RoutingLayer_T * layer, RouteStoredPacket_T * packet ) {
-	size_t		position;
-	RouteAddr_T	lastHopRouteAddr,
-				* list;
-	int			result,
-				count;
+	size_t		count,
+				position;
+	RouteAddr_T	* list,
+				lastHopRouteAddr;
+	int			result;
 
 	if( NULL == layer || NULL == packet || NULL == packet->Payload || 0 == packet->PayloadSize )
 		return FUNC_RESULT_FAILED_ARGUMENT;
 
 	list = ( RouteAddr_T * )( packet->Payload );
+	count = packet->PayloadSize / sizeof( RouteAddr_T );
+
 	result = helperChannel2Route( &( packet->LastHop ), &lastHopRouteAddr );
+	CHECK_RESULT( result );
 
-	if( FUNC_RESULT_SUCCESS != result )
-		return result;
+	if( routeAddrEqualPtr( &( packet->Source ), &lastHopRouteAddr ) )
+		position = count;
+	else
+		for( position = 0; position < count; position++ )
+			if( routeAddrEqualPtr( list + position, &lastHopRouteAddr ) )
+				break;
 
-	helperUpdateNeighbor( layer ); // TODO implement fine neighbor updating
+	if( 1 < position ) {
+		result = helperUpdateRouteAddrChainBefore( layer, list + position - 2, position - 1 );
+		CHECK_RESULT( result );
 
-	for( position = packet->PayloadSize / sizeof( RouteAddr_T ) - 1; position > 0; position-- )
-		if( routeAddrEqualPtr( list + position, &lastHopRouteAddr ) )
-			break;
+		result = helperUpdateRoute( layer, &( packet->Destination ), list );
+		CHECK_RESULT( result );
+		}
 
-	for( count = 0; position > 0; position-- ) {
-		result = helperUpdateRoute( layer, list + position + 1, list + position );
+	if( count > position ) {
+		result = helperUpdateRouteAddrChainAfter( layer, list + position, count - position - 1 );
+		CHECK_RESULT( result );
 
-		if( FUNC_RESULT_SUCCESS == result )
-			count++;
+		result = helperUpdateRoute( layer, &( packet->Source ), list + count - 1 );
 	}
 
-	return ( 0 < count ? FUNC_RESULT_SUCCESS : FUNC_RESULT_FAILED );
+	return result;
 }

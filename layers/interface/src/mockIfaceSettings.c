13596fb52c6c87e327afebdcd9756f646fdf1b7d
svalov <test>
2016-12-12 16:30:28 +0200

    Added mock iface settings binding function

---
+++ layers/interface/src/mockIfaceSettings.c
@@ -0,0 +1,21 @@
+//
+// Created by svalov on 12/12/16.
+//
+
+#include "mockIfaceSettings.h"
+#include "funcResults.h"
+
+int makeMockIfaceBinding(SettingsBind_T** binding, int* count){
+	if(NULL == binding || NULL == count)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	*count = 1;
+
+	SettingsBind_T* bind = malloc((*count)*sizeof(SettingsBind_T));
+	if(NULL == bind)
+		return FUNC_RESULT_FAILED_MEM_ALLOCATION;
+	*binding = bind;
+
+	BINDINGMAKE(bind++, mockIface, LogPath, FieldType_char);
+
+	return FUNC_RESULT_SUCCESS;
+}

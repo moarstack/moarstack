8e808f24ca8e5fbb47af99375b8c743e1ead3501
svalov <test>
2016-12-15 14:36:12 +0200

    Added header for data

--- layers/shared/inc/moarService.h
+++ layers/shared/inc/moarService.h
@@ -5,4 +5,15 @@
 #ifndef MOARSTACK_MOARSERVICE_H_H
 #define MOARSTACK_MOARSERVICE_H_H
 
+#include <moarApiCommon.h>
+#include <stdint.h>
+
+typedef uint16_t ServiceDataSize_T;
+
+typedef struct{
+	AppId_T LocalAppId;
+	AppId_T RemoteAppId;
+	ServiceDataSize_T PayloadSize;
+}ServiceLayerHeader_T;
+
 #endif //MOARSTACK_MOARSERVICE_H_H

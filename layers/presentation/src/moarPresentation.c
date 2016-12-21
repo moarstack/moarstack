2e52b33630843c87831f4342c4e113e3fda81265
svalov <test>
2016-12-21 13:52:49 +0200

    Added init functions

--- layers/presentation/src/moarPresentation.c
+++ layers/presentation/src/moarPresentation.c
@@ -13,14 +13,54 @@
 
 //init layer
 int presentationInit(PresentationLayer_T* layer, void* arg){
+	if(NULL == layer)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+	if(NULL == arg)
+		return FUNC_RESULT_FAILED_ARGUMENT;
 
+	MoarLayerStartupParams_T* params = (MoarLayerStartupParams_T*)arg;
+
+	layer->RoutingProcessingRules[0] = MakeProcessingRule(LayerCommandType_None, NULL);
+	//again
+	layer->ServiceProcessingRules[0] = MakeProcessingRule(LayerCommandType_None, NULL);
+	return FUNC_RESULT_SUCCESS;
 }
 //deinit layer
 int presentationDeinit(PresentationLayer_T* layer){
+	if(NULL == layer)
+		return FUNC_RESULT_FAILED_ARGUMENT;
 
+	return FUNC_RESULT_SUCCESS;
 }
 //init epoll
 int initEpoll(PresentationLayer_T* layer){
+	if(NULL == layer)
+		return FUNC_RESULT_FAILED_ARGUMENT;
+
+	// setup settings
+	layer->EpollCount = EPOLL_EVENTS_COUNT;
+	layer->EpollTimeout = EPOLL_TIMEOUT;
+	//epoll init here
+	memset(layer->EpollEvent,0,layer->EpollCount*sizeof(struct epoll_event));
+	//init
+	layer->EpollHandler = epoll_create(1);
+	if(-1 == layer->EpollHandler) {
+		return FUNC_RESULT_FAILED;
+	}
+	// add routing socket
+	struct epoll_event epollEventRouting;
+	epollEventRouting.events = EPOLL_ROUTING_EVENTS;
+	epollEventRouting.data.fd = layer->RoutingSocket;
+	int routingRes = epoll_ctl(layer->EpollHandler, EPOLL_CTL_ADD, layer->RoutingSocket, &epollEventRouting);
+	CHECK_RESULT(routingRes);
+	// add service socket
+	struct epoll_event epollEventService;
+	epollEventService.events = EPOLL_SERVICE_EVENTS;
+	epollEventService.data.fd = layer->ServiceSocket;
+	int serviceRes = epoll_ctl(layer->EpollHandler, EPOLL_CTL_ADD, layer->ServiceSocket, &epollEventService);
+	CHECK_RESULT(serviceRes);
+	//return
+	return FUNC_RESULT_SUCCESS;
 
 }
 
